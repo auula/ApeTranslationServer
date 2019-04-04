499f7522fdd973f51d1671537bfc0162a080d8ab
JDode <2420498526@qq.com>
2019-04-04 12:05:28 +0800

    add OCR API 🚲

---
+++ src/main/java/me/codegc/apet/server/controller/OCRAPI.java
@@ -0,0 +1,11 @@
+package me.codegc.apet.server.controller;
+
+/**
+ * Created by Ding on 2019/4/4 12:04 PM.
+ *
+ * @Description: OCR调用接口
+ * <p>
+ * <a href="https://github.com/JDode">to see GitHub</a>
+ */
+public class OCRAPI {
+}
--- src/main/java/me/codegc/apet/server/service/OCRService.java
+++ src/main/java/me/codegc/apet/server/service/OCRService.java
@@ -1,5 +1,11 @@
 package me.codegc.apet.server.service;
 
+import com.github.JDode.JTesseract.JTesseract;
+import com.github.JDode.JTesseract.model.Tesseract;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
 /**
  * Created by Ding on 2019/4/4 11:42 AM.
  *
@@ -7,6 +13,17 @@ package me.codegc.apet.server.service;
  * <p>
  * <a href="https://github.com/JDode">to see GitHub</a>
  */
+@Component
 public class OCRService {
 
+    private final String CONFIG = "/Users/jdode/Desktop/tesseract.properties";
+
+    //实例一个JTesseract对象传入相关参数
+    private final JTesseract init = new JTesseract().init(new Tesseract().build(CONFIG));
+
+    public String OCR(String file) {
+        //传入被识别文字的图片
+        init.setFile(new File(file));
+        return init.OCR();
+    }
 }
