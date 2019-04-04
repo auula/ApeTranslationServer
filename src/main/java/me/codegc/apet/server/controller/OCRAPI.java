b5e86d29fdf5f4188e380307fab368fe60439c92
JDode <2420498526@qq.com>
2019-04-04 12:15:10 +0800

    add update file service 🌹

--- src/main/java/me/codegc/apet/server/controller/OCRAPI.java
+++ src/main/java/me/codegc/apet/server/controller/OCRAPI.java
@@ -1,5 +1,9 @@
 package me.codegc.apet.server.controller;
 
+import me.codegc.apet.server.service.OCRService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Ding on 2019/4/4 12:04 PM.
  *
@@ -7,5 +11,13 @@ package me.codegc.apet.server.controller;
  * <p>
  * <a href="https://github.com/JDode">to see GitHub</a>
  */
+@RestController
 public class OCRAPI {
+
+    @Autowired
+    OCRService ocrService;
+
+    public String OCR() {
+        return ocrService.OCR("");
+    }
 }
---
+++ src/main/java/me/codegc/apet/server/service/ImageUploadService.java
@@ -0,0 +1,38 @@
+package me.codegc.apet.server.service;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Ding on 2019/4/4 12:10 PM.
+ *
+ * @Description: 文件上传service
+ * <p>
+ * <a href="https://github.com/JDode">to see GitHub</a>
+ */
+@Component
+public class ImageUploadService {
+    /**
+     * 功能：上传图片
+     * @param file 文件
+     * @param uploadPath 服务器上上传文件的路径
+     * @param physicalUploadPath  服务器上上传文件的物理路径
+     * @return 上传文件的 URL相对地址
+     */
+    public String uploadImage(MultipartFile file, String uploadPath, String physicalUploadPath ) {
+
+        String filePath = physicalUploadPath + file.getOriginalFilename();
+
+        try {
+            File targetFile=new File(filePath);
+            FileUtils.writeByteArrayToFile(targetFile, file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return uploadPath + "/" + file.getOriginalFilename();
+    }
+}
