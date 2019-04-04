1ffa8b31c0973ab3f21ab0aaa702fdbe55ed2114
JDode <2420498526@qq.com>
2019-04-04 11:56:10 +0800

    add JTesseract lib jar 😋

---
+++ src/main/java/me/codegc/apet/server/service/OCRService.java
@@ -0,0 +1,12 @@
+package me.codegc.apet.server.service;
+
+/**
+ * Created by Ding on 2019/4/4 11:42 AM.
+ *
+ * @Description: OCR服务类
+ * <p>
+ * <a href="https://github.com/JDode">to see GitHub</a>
+ */
+public class OCRService {
+
+}
