9082f0c5f11b5ad8d1d4ec4b43f9c1a71143535c
JDode <2420498526@qq.com>
2019-04-09 19:34:58 +0800

    add JTesseract jar to this project 👏

--- src/main/java/me/codegc/apet/server/services/OCRService.java
+++ src/main/java/me/codegc/apet/server/services/OCRService.java
@@ -1,6 +1,10 @@
 package me.codegc.apet.server.services;
 
+import com.github.JDode.JTesseract.JTesseract;
+import com.github.JDode.JTesseract.model.Tesseract;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 /**
  * Created by Ding on 2019/4/4 11:42 AM.
@@ -15,12 +19,12 @@ public class OCRService {
     private final String CONFIG = "/Users/jdode/Desktop/tesseract.properties";
 
     //实例一个JTesseract对象传入相关参数
-//    private final JTesseract init = new JTesseract().init(new Tesseract().build(CONFIG));
+    private final JTesseract init = new JTesseract().init(new Tesseract().build(CONFIG));
 
     public String OCR(String file) {
         //传入被识别文字的图片
-//        init.setFile(new File(file));
-//        return init.OCR();
-        return  "OCR服务器目前没有开放使用！！有问题联系Q2420498526";
+        init.setFile(new File(file));
+        return init.OCR();
+        //return  "OCR服务器目前没有开放使用！！有问题联系Q2420498526";
     }
 }
